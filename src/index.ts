b02a6e8fc834f4e8c04aac46d31727b85f52ba12
vdev3094 <66820680+vdev3094@users.noreply.github.com>
2022-07-22 23:37:06 +0300

    cors issue fixed

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,12 @@ import path from "path";
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
